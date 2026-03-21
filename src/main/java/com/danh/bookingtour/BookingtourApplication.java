4129c847aa34f51cba787917fe20a4ef66504bbc
Cao Tuan Danh <caotuandanh2103@gmail.com>
2026-03-21 15:06:33 +0700

    Initail booking tour project

---
+++ src/main/java/com/danh/bookingtour/BookingtourApplication.java
@@ -0,0 +1,13 @@
+package com.danh.bookingtour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingtourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingtourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danh/bookingtour/BookingtourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danh.bookingtour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingtourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
