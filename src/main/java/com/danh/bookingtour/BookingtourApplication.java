3e21ed8bf8faf8ad7c79e9e44a883db9b824bd10
DanhCT <tuandanh05082000@gmail.com>
2026-03-24 21:28:47 +0700

    config DB and security

--- src/main/java/com/danh/bookingtour/BookingtourApplication.java
+++ src/main/java/com/danh/bookingtour/BookingtourApplication.java
@@ -1,9 +1,11 @@
 package com.danh.bookingtour;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.danh.bookingtour.mapper")
 public class BookingtourApplication {
 
 	public static void main(String[] args) {
